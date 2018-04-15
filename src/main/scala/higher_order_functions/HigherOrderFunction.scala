a8fea36d2c7f791955cd50f25ba61fe05c4aa204
PeteGabriel <pedrogabriel@protonmail.ch>
2018-04-15 22:02:14 +0100

    End of Chapter 2 - Higher Order Functions

--- src/main/scala/higher_order_functions/HigherOrderFunction.scala
+++ src/main/scala/higher_order_functions/HigherOrderFunction.scala
@@ -21,12 +21,41 @@ object HigherOrderFunction {
     Your definition should use a local tail-recursive function.
   */
   def fib(n: Int): Int = {
+    /*
+    If all recursive calls made by a function are in tail position, Scala automatically compiles
+    the recursion to iterative loops that don’t consume call stack frames for each iteration.
+     */
     @tailrec
-    def loop(idx: Int, previous: Int, acc: Int): Int =
-      if (idx == n) acc
-      else loop(idx + 1, acc, acc + previous)
+    def loop(iteration: Int, previous: Int, acc: Int): Int =
+      if (iteration == n) acc
+      else loop(iteration + 1, acc, acc + previous)
 
     loop(0, 1, 0)
   }
 
+
+  def findFirst[A](source: Array[A], key: A): Int = {
+    @tailrec
+    def loop(iteration: Int): Int ={
+      if (iteration >= source.length) -1
+      else if (source(iteration) == key) iteration
+      else loop(iteration + 1)
+    }
+    loop(0)
+  }
+
+  def isSorted[A](source: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    def loop(iteration: Int, acc: Boolean): Boolean = {
+      if (iteration >= source.length) acc
+      else if (ordered(source(iteration - 1), source(iteration))) loop(iteration + 1, true)
+      else false
+    }
+    loop(1, false)
+  }
+
+  def curry[A,B,C](f: (A, B) => C): A => B => C = a => b => f(a,b)
+
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = (a,b) => f(a)(b)
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C = a => f(g(a))
 }
--- src/test/scala/HigherOrderTests.scala
+++ src/test/scala/HigherOrderTests.scala
@@ -1,6 +1,8 @@
 import org.scalatest.FunSuite
 import higher_order_functions.HigherOrderFunction.factorial
 import higher_order_functions.HigherOrderFunction.fib
+import higher_order_functions.HigherOrderFunction.findFirst
+import higher_order_functions.HigherOrderFunction.isSorted
 
 class HigherOrderTests extends FunSuite {
 
@@ -18,5 +20,29 @@ class HigherOrderTests extends FunSuite {
     assert(fib(6) == 8)
     assert(fib(7) == 13)
   }
+
+  test("First first element in array"){
+    val source = Array("1","2","3","4","5","6")
+    assert(findFirst(source, "4") == 3)
+  }
+
+  test("Return -1 if key is not present in array"){
+    val source = Array(1,2,3,4,5,6)
+    assert(findFirst(source, 14) == -1)
+  }
+
+  test("Validate if an array is sorted"){
+    var source = Array(1,2,3,4,5,6)
+    assert(isSorted(source, (a: Int, b: Int) => a <= b))
+
+    source = Array(1,3,4,2,6)
+    assert(!isSorted(source, (a: Int, b: Int) => a <= b))
+  }
+
+  test("Validate if an array with equal elements is sorted "){
+    val source = Array(1,1,1,1,1,1)
+    assert(isSorted(source, (a: Int, b: Int) => a <= b))
+  }
+
 
 }
